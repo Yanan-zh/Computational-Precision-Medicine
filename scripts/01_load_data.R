3f0d9cdd5429d430231f21a120ed0b176a6d3558
signemarcussen <engis-10@hotmail.com>
2022-06-13 15:06:54 +0200

    make file mapping probe to gene id

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 tab <- read.table("data/GSE60331_series_matrix.txt", sep = "\t", skip = 31, fill = TRUE)
 div <- which(tab[,1] == "!series_matrix_table_begin") # This line divides expr and pheno
+microarray_platform <- read.table("data/GPL15207-17536.txt", sep="\t", header = T, fill = T)
+
 
 # Expression data
 expr <- tab[(div+1):(nrow(tab)-1),]
@@ -18,6 +20,15 @@ pheno <- pheno %>%
     mutate(across(.fns = ~str_extract(.x, "(?<=:\\s)(.+)"))) %>% 
     rename(treatment_response = "treatment response")
 
+# Microarray platform data
+probeID_GeneSymbol <- microarray_platform %>% 
+    dplyr::select(ID, Gene.Symbol) %>% 
+    rename(probeID = ID,
+           GeneSymbol = Gene.Symbol) %>% 
+    mutate(GeneSymbol = str_split(GeneSymbol, pattern = " /// ")) %>% 
+    unnest(GeneSymbol)
+
 # Save data
 write.table(expr, "data/expr.txt", sep = "\t", row.names = TRUE, quote = FALSE)
 write.table(pheno, "data/pheno.txt", sep = "\t", row.names = FALSE, quote = TRUE)
+write.table(probeID_GeneSymbol, "data/probeID_GeneSymbol.txt", sep = "\t", row.names = F)
