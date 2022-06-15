23a662d95b42a250dafa2075fbd0d52d21631907
signemarcussen <engis-10@hotmail.com>
2022-06-15 11:27:57 +0200

    add code for pre-RvsNR

--- scripts/06_fgsea.R
+++ scripts/06_fgsea.R
@@ -4,46 +4,44 @@ library("fgsea")
 
 
 # Load data --------------------------------------------------------------------
-## File for mapping probe ID to gene ID
-probeID_gene <- read_tsv(file = "data/probeID_gene.tsv")
-
-## File with logFC for differentially expressed genes
-ranks_response_DEG <- read.table("data/response_geneset.txt", header = T)
 
 ## File with logFC for all genes
 ranks_response_all <- read.table("data/response_geneset_all.txt", header = T)
 
+## File with logFC for pre-treatment responder vs non-responder
+ranks_pre_R_vs_NR <- read.table("data/response_geneset_pre.tsv", header = T)
+
 ## File with GO BP gene sets
 GO_BP_genesets <- gmtPathways("data/GeneSymbols.gmt")
-
+#GO_entrez <- gmtPathways("data/Entrez.gmt")
 
 
 # Clean data -------------------------------------------------------------------
 
-## Prepare rank files
-
-### DEG
-ranks_response_DEG <- setNames(ranks_response_DEG$logFC, 
-                               ranks_response_DEG$GeneSymbol) # named vector
-
-### All
+## Ranks for all
 ranks_response_all <- ranks_response_all %>% 
     select(-adj.P.Val) %>% 
     arrange(desc(logFC)) 
 ranks_response_all <- setNames(ranks_response_all$logFC, 
                                ranks_response_all$GeneSymbol) # named vector
 
+## Ranks for pre R vs NR
+ranks_pre_R_vs_NR <- ranks_pre_R_vs_NR %>% 
+    select(-c(adj.P.Val, entrez_gene))
+ranks_pre_R_vs_NR <- setNames(ranks_pre_R_vs_NR$logFC, 
+                              ranks_pre_R_vs_NR$gene_symbol) # named vector
+#write.table(x = ranks_pre_R_vs_NR, file = "test.txt")
+
 
 # Run fGSEA analysis -----------------------------------------------------------
-fgseaRes_DEG <- fgsea(pathways = GO_BP_genesets, # list of gene sets to check
-                      stats    = ranks_response_DEG, # ranked named vector
-                      minSize  = 15, # minimum gene set size to include
-                      maxSize  = 500) # maximum gene set size to include
 fgseaRes_all <- fgsea(pathways = GO_BP_genesets, # list of gene sets to check
                       stats    = ranks_response_all, # ranked named vector
                       minSize  = 15, # minimum gene set size to include
                       maxSize  = 500) # maximum gene set size to include
-
+fgseaRes_pre_R_vs_NR <- fgsea(pathways = GO_BP_genesets, # list of gene sets to check
+                              stats    = ranks_pre_R_vs_NR, # ranked named vector
+                              minSize  = 15, # minimum gene set size to include
+                              maxSize  = 500) # maximum gene set size to include
 
 ## View most significant GO terms
 head(fgseaRes_all[order(pval), ])
